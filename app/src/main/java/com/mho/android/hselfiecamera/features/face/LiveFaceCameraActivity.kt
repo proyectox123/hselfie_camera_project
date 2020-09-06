a061932da24b6de0c12526eb6ab2ab9ded30ae5e
MHO <mauricio.hdez.o.10@gmail.com>
2020-09-06 17:07:06 -0500

    Refactor in LiveFaceCameraViewModel.

--- app/src/main/java/com/mho/android/hselfiecamera/features/face/LiveFaceCameraActivity.kt
+++ app/src/main/java/com/mho/android/hselfiecamera/features/face/LiveFaceCameraActivity.kt
@@ -1,81 +1,53 @@
 package com.mho.android.hselfiecamera.features.face
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Handler
-import android.os.Message
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.huawei.hms.mlsdk.MLAnalyzerFactory
-import com.huawei.hms.mlsdk.common.LensEngine
-import com.huawei.hms.mlsdk.common.MLAnalyzer
-import com.huawei.hms.mlsdk.common.MLResultTrailer
+import androidx.lifecycle.Observer
 import com.huawei.hms.mlsdk.face.MLFace
-import com.huawei.hms.mlsdk.face.MLFaceAnalyzer
-import com.huawei.hms.mlsdk.face.MLFaceAnalyzerSetting
-import com.huawei.hms.mlsdk.face.MLMaxSizeFaceTransactor
 import com.mho.android.hselfiecamera.R
 import com.mho.android.hselfiecamera.overlay.LocalFaceGraphic
 import com.mho.android.hselfiecamera.usecases.SaveImageIntoGalleryUseCase
-import com.mho.android.hselfiecamera.utils.Constants.BUNDLE_KEY_LENS_TYPE
-import com.mho.android.hselfiecamera.utils.Constants.EXTRA_NAME_DETECT_MODE_CODE
-import com.mho.android.hselfiecamera.utils.Constants.EXTRA_VALUES_DETECT_MODE_CODE_NEAREST_PEOPLE
 import com.mho.android.hselfiecamera.utils.getViewModel
 import kotlinx.android.synthetic.main.activity_live_face_camera.*
-import java.io.IOException
 
 class LiveFaceCameraActivity : AppCompatActivity() {
-
-    private var analyzer: MLFaceAnalyzer? = null
-    private var lensEngine: LensEngine? = null
-    private var lensType = LensEngine.FRONT_LENS
-    private var detectModeCode = 0
-    private val smilingRate = 0.8F
-    private val smilingPossibility = 0.95F
-    private var isSafeToTakePicture = false
 
     private val saveImageIntoGalleryUseCase: SaveImageIntoGalleryUseCase by lazy {
         SaveImageIntoGalleryUseCase(contentResolver)
     }
 
     private val liveFaceCameraViewModel: LiveFaceCameraViewModel by lazy {
-        getViewModel { LiveFaceCameraViewModel(saveImageIntoGalleryUseCase) }
+        getViewModel {
+            LiveFaceCameraViewModel(
+                application,
+                saveImageIntoGalleryUseCase,
+                { obj -> addGraphicToOverlay(obj) },
+                { clearOverlay() }
+            )
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_live_face_camera)
-        if(savedInstanceState != null){
-            lensType = savedInstanceState.getInt(BUNDLE_KEY_LENS_TYPE)
-        }
 
-        try {
-            detectModeCode = intent.getIntExtra(EXTRA_NAME_DETECT_MODE_CODE, 1)
-        }catch (e: RuntimeException) {
-            Log.e("Error: ", "No detection code available.")
-        }
+        liveFaceCameraViewModel.onValidateSavedInstanceState(savedInstanceState)
+        liveFaceCameraViewModel.onValidateExtras(intent)
 
-        facingSwitch.setOnClickListener { view ->
-            lensType = if(lensType == LensEngine.FRONT_LENS){
-                LensEngine.BACK_LENS
-            }else{
-                LensEngine.FRONT_LENS
-            }
+        liveFaceCameraViewModel.events.observe(this, Observer { event ->
+            event.getContentIfNotHandled()?.let { validateNavigation(it) }
+        })
 
-            lensEngine?.close()
+        facingSwitch.setOnClickListener { liveFaceCameraViewModel.onFacingSwitch() }
+        restart.setOnClickListener { liveFaceCameraViewModel.onStartPreview() }
 
-            startPreview()
-        }
-        restart.setOnClickListener { startPreview() }
-
-        createFaceAnalyzer()
-        createLensEngine()
+        liveFaceCameraViewModel.onStartFaceAnalyzer()
     }
 
     override fun onResume() {
         super.onResume()
-        startLensEngine()
+        liveFaceCameraViewModel.onStartLensEngine()
     }
 
     override fun onPause() {
@@ -83,164 +55,37 @@ class LiveFaceCameraActivity : AppCompatActivity() {
         preview.stop()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        lensEngine?.release()
-    }
-
     override fun onSaveInstanceState(outState: Bundle) {
-        outState.putInt(BUNDLE_KEY_LENS_TYPE, lensType)
+        liveFaceCameraViewModel.onSaveInstanceState(outState)
         super.onSaveInstanceState(outState)
     }
 
-    private fun createLensEngine(){
-        val context = applicationContext
-        lensEngine = LensEngine.Creator(context, analyzer)
-            .setLensType(lensType)
-            .applyDisplayDimension(640, 480)
-            .applyFps(25.0F)
-            .enableAutomaticFocus(true)
-            .create()
-
+    private fun clearOverlay(){
+        faceOverlay.clear()
     }
 
-    private fun createFaceAnalyzer(){
-        val setting = MLFaceAnalyzerSetting.Factory()
-            .setFeatureType(MLFaceAnalyzerSetting.TYPE_FEATURES)
-            .setKeyPointType(MLFaceAnalyzerSetting.TYPE_UNSUPPORT_KEYPOINTS)
-            .setMinFaceProportion(0.1F)
-            .setTracingAllowed(true)
-            .create()
-
-        analyzer = MLAnalyzerFactory.getInstance().getFaceAnalyzer(setting)
-        if(detectModeCode == EXTRA_VALUES_DETECT_MODE_CODE_NEAREST_PEOPLE){
-            val transactor = MLMaxSizeFaceTransactor.Creator(analyzer, object: MLResultTrailer<MLFace?>(){
-                override fun objectCreateCallback(itemId: Int, obj: MLFace?) {
-                    faceOverlay.clear()
-                    if(obj == null){
-                        return
-                    }
-
-                    val faceGraphic = LocalFaceGraphic(faceOverlay, obj, this@LiveFaceCameraActivity)
-                    faceOverlay.addGraphic(faceGraphic)
-
-                    val emotion = obj.emotions
-                    if(emotion.smilingProbability >= smilingPossibility){
-                        isSafeToTakePicture = true
-                        handler.sendEmptyMessage(ACTION_TAKE_PHOTO)
-                    }
-                }
-
-                override fun objectUpdateCallback(var1: MLAnalyzer.Result<MLFace?>?, obj: MLFace?) {
-                    faceOverlay.clear()
-                    if(obj == null){
-                        return
-                    }
-
-                    val faceGraphic = LocalFaceGraphic(faceOverlay, obj, this@LiveFaceCameraActivity)
-                    faceOverlay.addGraphic(faceGraphic)
-
-                    val emotion = obj.emotions
-                    if(emotion.smilingProbability >= smilingPossibility && isSafeToTakePicture){
-                        isSafeToTakePicture = false
-                        handler.sendEmptyMessage(ACTION_TAKE_PHOTO)
-                    }
-                }
-
-                override fun lostCallback(result: MLAnalyzer.Result<MLFace?>?) {
-                    faceOverlay.clear()
-                }
-
-                override fun completeCallback() {
-                    faceOverlay.clear()
-                }
-            }).create()
-            analyzer?.setTransactor(transactor)
-        } else {
-            analyzer?.setTransactor(object: MLAnalyzer.MLTransactor<MLFace> {
-                override fun transactResult(result: MLAnalyzer.Result<MLFace>) {
-                    val faceSparseArray = result.analyseList
-                    var flag = 0
-                    for(i in 0 until faceSparseArray.size()){
-                        val emotion = faceSparseArray.valueAt(i).emotions
-                        if(emotion.smilingProbability >= smilingPossibility){
-                            flag++
-                        }
-                    }
-
-                    if(flag > faceSparseArray.size() * smilingRate && isSafeToTakePicture){
-                        isSafeToTakePicture = false
-                        handler.sendEmptyMessage(ACTION_TAKE_PHOTO)
-                    }
-                }
-
-                override fun destroy() { }
-            })
-        }
+    private fun addGraphicToOverlay(obj: MLFace){
+        val faceGraphic = LocalFaceGraphic(faceOverlay, obj, this@LiveFaceCameraActivity)
+        faceOverlay.addGraphic(faceGraphic)
     }
 
-    private fun startLensEngine(){
-        restart.visibility = View.GONE
-        if(lensEngine != null){
-            try {
-                if(detectModeCode == EXTRA_VALUES_DETECT_MODE_CODE_NEAREST_PEOPLE){
-                    preview.start(lensEngine, faceOverlay)
-                }else {
-                    preview.start(lensEngine)
-                }
-
-                isSafeToTakePicture = true
-            }catch (e: IOException){
-                lensEngine?.release()
-                lensEngine = null
+    private fun validateNavigation(navigation: LiveFaceCameraViewModel.LiveFaceCameraNavigation) {
+        when (navigation) {
+            LiveFaceCameraViewModel.LiveFaceCameraNavigation.ShowRestartOption -> {
+                restart.visibility = View.VISIBLE
+            }
+            LiveFaceCameraViewModel.LiveFaceCameraNavigation.ReleasePreview -> {
+                preview.release()
+            }
+            LiveFaceCameraViewModel.LiveFaceCameraNavigation.HideRestartOption -> {
+                restart.visibility = View.GONE
+            }
+            is LiveFaceCameraViewModel.LiveFaceCameraNavigation.StartPreviewMostPeople -> navigation.run {
+                preview.start(lensEngine)
+            }
+            is LiveFaceCameraViewModel.LiveFaceCameraNavigation.StartPreviewNearestPeople -> navigation.run {
+                preview.start(lensEngine, faceOverlay)
             }
         }
-    }
-
-    private fun startPreview(){
-        preview.release()
-        createFaceAnalyzer()
-        createLensEngine()
-        startLensEngine()
-    }
-
-    private fun stopPreview(){
-        restart.visibility = View.VISIBLE
-        lensEngine?.release()
-        isSafeToTakePicture = false
-
-        if(analyzer != null){
-            try {
-                analyzer!!.stop()
-            }catch(e: IOException){
-                Log.e("Error: ", "It's not possible to stop the camera. :(")
-            }
-        }
-    }
-
-    private fun takePhoto(){
-        lensEngine?.photograph(null) { bytes ->
-            handler.sendEmptyMessage(ACTION_STOP_PREVIEW)
-            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-
-            liveFaceCameraViewModel.onSaveImageIntoGallery(bitmap)
-        }
-    }
-
-    private val handler: Handler = object: Handler() {
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            when(msg.what){
-                ACTION_STOP_PREVIEW -> stopPreview()
-                ACTION_TAKE_PHOTO -> takePhoto()
-                else -> { }
-            }
-        }
-    }
-
-    companion object {
-
-        private const val ACTION_STOP_PREVIEW = 1
-        private const val ACTION_TAKE_PHOTO = 2
     }
 }
--- app/src/main/java/com/mho/android/hselfiecamera/features/face/LiveFaceCameraViewModel.kt
+++ app/src/main/java/com/mho/android/hselfiecamera/features/face/LiveFaceCameraViewModel.kt
@@ -1,14 +1,218 @@
 package com.mho.android.hselfiecamera.features.face
 
-import android.graphics.Bitmap
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.huawei.hms.mlsdk.MLAnalyzerFactory
+import com.huawei.hms.mlsdk.common.LensEngine
+import com.huawei.hms.mlsdk.common.MLAnalyzer
+import com.huawei.hms.mlsdk.common.MLResultTrailer
+import com.huawei.hms.mlsdk.face.MLFace
+import com.huawei.hms.mlsdk.face.MLFaceAnalyzer
+import com.huawei.hms.mlsdk.face.MLFaceAnalyzerSetting
+import com.huawei.hms.mlsdk.face.MLMaxSizeFaceTransactor
+import com.mho.android.hselfiecamera.features.face.LiveFaceCameraViewModel.LiveFaceCameraNavigation.*
 import com.mho.android.hselfiecamera.usecases.SaveImageIntoGalleryUseCase
+import com.mho.android.hselfiecamera.utils.Constants
+import com.mho.android.hselfiecamera.utils.Constants.EXTRA_VALUES_DETECT_MODE_CODE_NEAREST_PEOPLE
+import com.mho.android.hselfiecamera.utils.Event
+import java.io.IOException
 
 class LiveFaceCameraViewModel(
-    private val saveImageIntoGalleryUseCase: SaveImageIntoGalleryUseCase
-): ViewModel(){
+    application: Application,
+    private val saveImageIntoGalleryUseCase: SaveImageIntoGalleryUseCase,
+    private val addGraphicToOverlay: (obj: MLFace) -> Unit,
+    private val clearOverlay: () -> Unit
+): AndroidViewModel(application){
 
-    fun onSaveImageIntoGallery(bitmap: Bitmap){
-        saveImageIntoGalleryUseCase.invoke(bitmap)
+    val smilingRate = 0.8F
+    val smilingPossibility = 0.95F
+    var detectModeCode = 0
+    var lensType = LensEngine.FRONT_LENS
+    var lensEngine: LensEngine? = null
+    var isSafeToTakePicture = false
+
+    private val analyzer: MLFaceAnalyzer by lazy {
+        val setting = MLFaceAnalyzerSetting.Factory()
+            .setFeatureType(MLFaceAnalyzerSetting.TYPE_FEATURES)
+            .setKeyPointType(MLFaceAnalyzerSetting.TYPE_UNSUPPORT_KEYPOINTS)
+            .setMinFaceProportion(0.1F)
+            .setTracingAllowed(true)
+            .create()
+
+        MLAnalyzerFactory.getInstance().getFaceAnalyzer(setting)
+    }
+
+    private val _events = MutableLiveData<Event<LiveFaceCameraNavigation>>()
+    val events: LiveData<Event<LiveFaceCameraNavigation>> get() = _events
+
+    override fun onCleared() {
+        super.onCleared()
+        lensEngine?.release()
+    }
+
+    fun onValidateSavedInstanceState(savedInstanceState: Bundle?) {
+        lensType = savedInstanceState?.getInt(Constants.BUNDLE_KEY_LENS_TYPE) ?: LensEngine.FRONT_LENS
+    }
+
+    fun onSaveInstanceState(outState: Bundle){
+        outState.putInt(Constants.BUNDLE_KEY_LENS_TYPE, lensType)
+    }
+
+    fun onValidateExtras(intent: Intent){
+        try {
+            detectModeCode = intent.getIntExtra(Constants.EXTRA_NAME_DETECT_MODE_CODE, 1)
+        }catch (e: RuntimeException) {
+            Log.e("Error: ", "No detection code available.")
+        }
+    }
+
+    fun onTakePhoto(){
+        lensEngine?.photograph(null) { bytes ->
+            stopPreview()
+
+            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            saveImageIntoGalleryUseCase.invoke(bitmap)
+        }
+    }
+
+    fun onStartFaceAnalyzer() {
+        createFaceAnalyzer()
+        createLensEngine()
+    }
+
+    fun onStartPreview(){
+        onStartFaceAnalyzer()
+        onStartLensEngine()
+    }
+
+    fun onStartLensEngine(){
+        _events.value = Event(HideRestartOption)
+        if(lensEngine != null){
+            try {
+                if(detectModeCode == EXTRA_VALUES_DETECT_MODE_CODE_NEAREST_PEOPLE){
+                    _events.value = Event(StartPreviewNearestPeople(lensEngine))
+                }else {
+                    _events.value = Event(StartPreviewMostPeople(lensEngine))
+                }
+
+                isSafeToTakePicture = true
+            }catch (e: IOException){
+                lensEngine?.release()
+                lensEngine = null
+            }
+        }
+    }
+
+    fun onFacingSwitch(){
+        lensType = when (lensType) {
+            LensEngine.FRONT_LENS -> LensEngine.BACK_LENS
+            else -> LensEngine.FRONT_LENS
+        }
+
+        lensEngine?.close()
+
+        onStartPreview()
+    }
+
+    private fun createFaceAnalyzer(){
+        when (detectModeCode) {
+            EXTRA_VALUES_DETECT_MODE_CODE_NEAREST_PEOPLE -> {
+                analyzer.setTransactor(transactorNearestPeople())
+            }
+            else -> {
+                analyzer.setTransactor(transactorMostPeople())
+            }
+        }
+    }
+
+    private fun transactorNearestPeople() = MLMaxSizeFaceTransactor.Creator(analyzer, object: MLResultTrailer<MLFace?>(){
+        override fun objectCreateCallback(itemId: Int, obj: MLFace?) {
+            clearOverlay()
+            if(obj == null){
+                return
+            }
+
+            addGraphicToOverlay(obj)
+
+            val emotion = obj.emotions
+            if(emotion.smilingProbability >= smilingPossibility){
+                isSafeToTakePicture = true
+                onTakePhoto()
+            }
+        }
+
+        override fun objectUpdateCallback(var1: MLAnalyzer.Result<MLFace?>?, obj: MLFace?) {
+            clearOverlay()
+            if(obj == null){
+                return
+            }
+
+            addGraphicToOverlay(obj)
+
+            val emotion = obj.emotions
+            if(emotion.smilingProbability >= smilingPossibility && isSafeToTakePicture){
+                isSafeToTakePicture = false
+                onTakePhoto()
+            }
+        }
+
+        override fun lostCallback(result: MLAnalyzer.Result<MLFace?>?) {
+            clearOverlay()
+        }
+
+        override fun completeCallback() {
+            clearOverlay()
+        }
+    }).create()
+
+    private fun transactorMostPeople() = object : MLAnalyzer.MLTransactor<MLFace> {
+        override fun transactResult(result: MLAnalyzer.Result<MLFace>) {
+            val faceSparseArray = result.analyseList
+            val flag = (0 until faceSparseArray.size())
+                .map { faceSparseArray.valueAt(it).emotions }
+                .count { it.smilingProbability >= smilingPossibility }
+
+            if (flag > faceSparseArray.size() * smilingRate && isSafeToTakePicture) {
+                isSafeToTakePicture = false
+                onTakePhoto()
+            }
+        }
+
+        override fun destroy() { }
+    }
+
+    private fun createLensEngine(){
+        lensEngine = LensEngine.Creator(getApplication(), analyzer)
+            .setLensType(lensType)
+            .applyDisplayDimension(640, 480)
+            .applyFps(25.0F)
+            .enableAutomaticFocus(true)
+            .create()
+    }
+
+    private fun stopPreview(){
+        _events.value = Event(ShowRestartOption)
+        lensEngine?.release()
+        isSafeToTakePicture = false
+
+        try {
+            analyzer.stop()
+        }catch(e: IOException){
+            Log.e("Error: ", "It's not possible to stop the camera. :(")
+        }
+    }
+
+    sealed class LiveFaceCameraNavigation {
+        data class StartPreviewMostPeople(val lensEngine: LensEngine?) : LiveFaceCameraNavigation()
+        data class StartPreviewNearestPeople(val lensEngine: LensEngine?) : LiveFaceCameraNavigation()
+        object ReleasePreview: LiveFaceCameraNavigation()
+        object ShowRestartOption: LiveFaceCameraNavigation()
+        object HideRestartOption: LiveFaceCameraNavigation()
     }
 }
